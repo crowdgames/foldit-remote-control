ea226d3a5dd3fc64e70624aa5edda48f969fe7d6
ltrowel <trowel.liam@gmail.com>
2015-04-15 12:56:55 -0400

    All tests written, problem setting up StreamView mocks though

--- RemoteControlAndroid/app/src/androidTest/java/it/fold/remotecontrolandroid/ApplicationTest.java
+++ RemoteControlAndroid/app/src/androidTest/java/it/fold/remotecontrolandroid/ApplicationTest.java
@@ -1,9 +1,13 @@
 package it.fold.remotecontrolandroid;
 
 import android.app.Application;
+import android.graphics.PointF;
 import android.os.SystemClock;
 import android.test.ApplicationTestCase;
 import android.view.MotionEvent;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.mockito.Mockito.*;
 
@@ -19,11 +24,22 @@ import static org.mockito.Mockito.*;
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
 
-    @Mock public StreamView _streamView;
+    @Mock public Context _context;
 
-    public MotionEvent _motionEventDownOne = MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN, 100, 200, 0);
-    public MotionEvent _motionEventDownTwo = MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN, 500, 500, 0);
-    public MotionEvent _motionEventDownThree = MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN, 300, 100, 0);
+    @Mock public AttributeSet _attributeSet;
+
+    public StreamView _streamView;
+
+    public long downTime1;
+    public long downTime2;
+    public long downTime3;
+    public long eventTime1;
+    public long eventTime2;
+    public long eventTime3;
+
+    public MotionEvent _motionEventDownOne;
+    public MotionEvent _motionEventDownTwo;
+    public MotionEvent _motionEventDownThree;
 
     /**
      * @Author Conor Ebbs
@@ -39,6 +55,7 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         System.setProperty("dexmaker.dexcache", getContext().getCacheDir().getPath());
 
         MockitoAnnotations.initMocks(this);
+        _streamView = new StreamView(_context, _attributeSet);
     }
 
     public void testStatesAfterInit() {
@@ -47,18 +64,41 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
 //        System.out.println("test");
     }
 
-    public void testOnTouchEventSingle() {
-        assertTrue(_streamView.onTouchEvent(_motionEventDownOne)); // Should not fail
+    public void testOnTouchEvent() {
+        //Create first motionEvent
+        downTime1 = SystemClock.uptimeMillis();
+        eventTime1 = SystemClock.uptimeMillis();
+        _motionEventDownOne = MotionEvent.obtain(downTime1, eventTime1, MotionEvent.ACTION_DOWN, 100, 200, 0);
         _streamView.onTouchEvent(_motionEventDownOne);
-        //Assert there is one pointer
+        //Check that it exists
+        assertEquals(1, _streamView._pointers.size());
+        //Create second motionEvent
+        downTime2 = SystemClock.uptimeMillis();
+        eventTime2 = SystemClock.uptimeMillis();
+        _motionEventDownTwo = MotionEvent.obtain(downTime2, eventTime2, MotionEvent.ACTION_DOWN, 500, 500, 0);
+        _streamView.onTouchEvent(_motionEventDownTwo);
+        //Check that it exists
+        assertEquals(2, _streamView._pointers.size());
+        //Move first motionEvent
+        eventTime1 = SystemClock.uptimeMillis();
+        _motionEventDownOne = MotionEvent.obtain(downTime1, eventTime1, MotionEvent.ACTION_MOVE, 1200, 300, 0);
+        _streamView.onTouchEvent(_motionEventDownOne);
+        //Check that it moved
+        assertEquals(1200, _streamView._pointers.get(1).x);
+        assertEquals(300, _streamView._pointers.get(1).y);
+        //Create third motionEvent
+        downTime3 = SystemClock.uptimeMillis();
+        eventTime3 = SystemClock.uptimeMillis();
+        _motionEventDownTwo = MotionEvent.obtain(downTime3, eventTime3, MotionEvent.ACTION_DOWN, 300, 100, 0);
+        _streamView.onTouchEvent(_motionEventDownThree);
+        //Check that it was added
+        assertEquals(3, _streamView._pointers.size());
+        //Remove second motionEvent
+        eventTime2 = SystemClock.uptimeMillis();
+        _motionEventDownTwo = MotionEvent.obtain(downTime2, eventTime2, MotionEvent.ACTION_UP, 500, 500, 0);
         _streamView.onTouchEvent(_motionEventDownTwo);
         //Assert there are two pointers
-        //Move _motionEventDownOne
-        //Assert _motionEventDownOne moved
-        _streamView.onTouchEvent(_motionEventDownThree);
-        //Assert there are three pointers
-        //Remove _motionEventDownTwo
-        //Assert there are two pointers
+        assertEquals(2, _streamView._pointers.size());
     }
 
 }
