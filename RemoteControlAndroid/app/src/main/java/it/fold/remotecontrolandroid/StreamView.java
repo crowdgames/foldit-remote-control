37a17bf0ccf9f3b68c2f7725fb33571c5373881a
Conor Ebbs <ebbs.c@husky.neu.edu>
2015-04-20 17:32:21 -0400

    Cleaning up of code in StreamView

--- RemoteControlAndroid/app/src/main/java/it/fold/remotecontrolandroid/StreamView.java
+++ RemoteControlAndroid/app/src/main/java/it/fold/remotecontrolandroid/StreamView.java
@@ -111,198 +111,22 @@ public class StreamView extends SurfaceView implements SurfaceHolder.Callback {
         Log.d("StreamView", "surfaceChanged");
     }
 
+    @Override
     /**
      * Handles touch events from the user
      *
-     * @param e information about event
-     * @return boolean true
+     * @param e MotionEvent from android
+     * @return
      */
-//        public boolean onTouchEvent(MotionEvent e) {
-//
-//            mGestureDetector.onTouchEvent(e);
-//            int action = e.getAction();
-//            int cl_action = 0;
-//
-//            int maskedAction = e.getActionMasked();
-//            int pointerIndex = e.getActionIndex();
-//            int pointerId = e.getPointerId(pointerIndex);
-//            int x = (int) e.getX();
-//            int y = (int) e.getY();
-//
-//            switch(action) {
-//                case MotionEvent.ACTION_DOWN: {
-//                    Log.d("debug", "ACTION_DOWN");
-//                    cl_action = Constants.CLEV_MOUSE_DOWN;
-//                }
-//                case MotionEvent.ACTION_POINTER_DOWN: {
-//                    Log.d("debug", "ACTION_POINTER_ DOWN");
-//                    // New pointer
-//
-//                    PointF f = new PointF();
-//                    f.x = e.getX(pointerIndex);
-//                    f.y = e.getY(pointerIndex);
-//                    mActivePointers.put(pointerId, f);
-//
-//                    Log.d("debug", "f.x: " + f.x + " ::: f.y: " + f.y);
-//                    break;
-//                }
-//                case MotionEvent.ACTION_MOVE: { // a pointer was moved
-//                    Log.d("debug", "ACTION_MOVE");
-////                    cl_action = Constants.CLEV_MOUSE_MOVE;
-//                    if(e.getPointerCount() > 1) {
-//                        for (int size = e.getPointerCount(), i = 0; i < size; i++) {
-//                            PointF point = mActivePointers.get(e.getPointerId(i));
-//                            if (point != null) {
-//                                int pointId = e.getPointerId(i);
-//                                point.x = e.getX(i);
-//                                point.y = e.getY(i);
-//                                Log.d("debug", "POINT ID:: " + pointId + " point.x: " + point.x + " ::: point.y: " + point.y);
-//                                switch (pointId) {
-//                                    case 0: cl_action = Constants.CLEV_MOUSE_DOWN_AUX_0;
-//                                    case 1: cl_action = Constants.CLEV_MOUSE_DOWN_AUX_1;
-//                                    case 2: cl_action = Constants.CLEV_MOUSE_DOWN_AUX_2;
-//                                }
-//                             mStreamThreadHandler.obtainMessage(cl_action, ((int) point.x), ((int) point.y)).sendToTarget();
-//                            }
-//                        }
-//                    } else {
-//                            cl_action = Constants.CLEV_MOUSE_MOVE;
-//                            mStreamThreadHandler.obtainMessage(cl_action, x, y).sendToTarget();
-//                    }
-//    //                break;
-//                    return mScaleGestureDetector.onTouchEvent(e);
-//                }
-//            case MotionEvent.ACTION_UP: {
-//                cl_action = Constants.CLEV_MOUSE_UP;
-//            }
-//            case MotionEvent.ACTION_POINTER_UP: {
-//
-//            }
-//            case MotionEvent.ACTION_CANCEL: {
-//                Log.d("debug", "pointer removed");
-//                mActivePointers.remove(pointerId);
-//                break;
-//            }
-//            case MotionEvent.ACTION_SCROLL: {
-//                if (android.os.Build.VERSION.SDK_INT >= 12) {
-//                    if (e.getAxisValue(MotionEvent.AXIS_VSCROLL) < 0) {
-//                        cl_action = Constants.CLEV_SCROLL_DOWN;
-//                    } else {
-//                        cl_action = Constants.CLEV_SCROLL_UP;
-//                    }
-//                }
-//            }
-//            }
-//            invalidate();
-//            Log.d("debug", "--");
-//
-//            return true;
-//        }
-    @Override
     public boolean onTouchEvent(MotionEvent e) {
-        //mScaleDetector.onTouchEvent(e);
-        //touchTime = e.getEventTime();
+
         mGestureDetector.onTouchEvent(e);
-//        if (e.getPointerCount() > 1) {
-//            return mScaleGestureDetector.onTouchEvent(e);
-//        }
-        int x = (int) e.getX();
-        int y = (int) e.getY();
-        int action = e.getAction();
-        int maskedAction = e.getActionMasked();
-        int pointerCount = e.getPointerCount();
-        int cl_action = 0;
-//        Log.d("debug", "action = " + action);
-//        Log.d("debug", "maskedAction = " + maskedAction);
-//        Log.d("debug", "pointer count = " + pointerCount);
 
-        if (pointerCount > 1) {
-            if (maskedAction == MotionEvent.ACTION_POINTER_DOWN) {
-                Log.d("debug", "action pointer down");
-                for (int size = pointerCount, i = 0; i < size; i++) {
-                    int pointId = e.getPointerId(i);
-                    x = (int) e.getX(i);
-                    y = (int) e.getY(i);
-                    addPointer(pointId, x, y);
-//                    Log.d("debug", "POINT ID:: " + pointId + " point.x: " + x + " ::: point.y: " + y);
-                    switch (pointId) {
-                        case 0:
-                            cl_action = Constants.CLEV_MOUSE_DOWN_AUX_0;
-                        case 1:
-                            cl_action = Constants.CLEV_MOUSE_DOWN_AUX_1;
-                        case 2:
-                            cl_action = Constants.CLEV_MOUSE_DOWN_AUX_2;
-                    }
-                    Log.d("debug", "Sending ::POINTER DOWN :: " + cl_action + " through the stream...");
-                    mStreamThreadHandler.obtainMessage(cl_action, ((int) x), ((int) y)).sendToTarget();
-                }
-//                Log.d("debug", "ACTION POINTER DOWN ::::::::::::::::::::::::::::::: DOWN:");
-            } else if (maskedAction == MotionEvent.ACTION_POINTER_UP) {
-                Log.d("debug", "action pointer up");
-                for (int size = pointerCount, i = 0; i < size; i++) {
-                    int pointId = e.getPointerId(i);
-                    x = (int) e.getX(i);
-                    y = (int) e.getY(i);
-                    removePointer(pointId);
-//                        Log.d("debug", "POINT ID:: " + pointId + " point.x: " + point.x + " ::: point.y: " + point.y);
-                    switch (pointId) {
-                        case 0:
-                            cl_action = Constants.CLEV_MOUSE_UP_AUX_0;
-                        case 1:
-                            cl_action = Constants.CLEV_MOUSE_UP_AUX_1;
-                        case 2:
-                            cl_action = Constants.CLEV_MOUSE_UP_AUX_2;
-                    }
-                    Log.d("debug", "Sending :: POINTER UP :: " + cl_action + " through the stream...");
-                    mStreamThreadHandler.obtainMessage(cl_action, ((int) x), ((int) y)).sendToTarget();
-
-                }
-//                Log.d("debug", "ACTION POINTER UP :::::::::::::::::::::::::::::::: UP");
-            } else if (maskedAction == MotionEvent.ACTION_MOVE) {
-                Log.d("debug", "action move");
-                for (int size = pointerCount, i = 0; i < size; i++) {
-                    int pointId = e.getPointerId(i);
-                    x = (int) e.getX(i);
-                    y = (int) e.getY(i);
-                    addPointer(pointId, x, y);
-//                        Log.d("debug", "POINT ID:: " + pointId + " point.x: " + point.x + " ::: point.y: " + point.y);
-                    switch (pointId) {
-                        case 0:
-                            cl_action = Constants.CLEV_MOUSE_MOVE_AUX_0;
-                        case 1:
-                            cl_action = Constants.CLEV_MOUSE_MOVE_AUX_1;
-                        case 2:
-                            cl_action = Constants.CLEV_MOUSE_MOVE_AUX_2;
-                    }
-                    Log.d("debug", "Sending :: MOVE :: " + cl_action + " through the stream...");
-                    mStreamThreadHandler.obtainMessage(cl_action, ((int) x), ((int) y)).sendToTarget();
-
-                }
-            } else {
-                Log.d("debug", "no case triggered for multitouch");
-                return true;
-            }
+        if (e.getPointerCount() > 1) {
+            return handleMultiplePointers(e);
         } else {
-            if (action == MotionEvent.ACTION_DOWN) {
-                cl_action = Constants.CLEV_MOUSE_DOWN;
-            } else if (action == MotionEvent.ACTION_UP) {
-                cl_action = Constants.CLEV_MOUSE_UP;
-            } else if (action == MotionEvent.ACTION_MOVE) {
-                cl_action = Constants.CLEV_MOUSE_MOVE;
-            } else if (action == MotionEvent.ACTION_SCROLL) {
-                if (android.os.Build.VERSION.SDK_INT >= 12) {
-                    if (e.getAxisValue(MotionEvent.AXIS_VSCROLL) < 0) {
-                        cl_action = Constants.CLEV_SCROLL_DOWN;
-                    } else {
-                        cl_action = Constants.CLEV_SCROLL_UP;
-                    }
-                }
-            } else {
-                return true;
-            }
+            return handleSinglePointer(e);
         }
-        mStreamThreadHandler.obtainMessage(cl_action, x, y).sendToTarget();
-        return true;
     }
 
     /**
@@ -334,4 +158,117 @@ public class StreamView extends SurfaceView implements SurfaceHolder.Callback {
             Log.d("error", "List of pointers was never initialized. Cannot remove pointer.");
         }
     }
+
+    /**
+     * Handles multiple pointers in the form of the passed in MotionEvent
+     *
+     * @param e MotionEvent registered from android
+     * @return
+     */
+    public boolean handleMultiplePointers(MotionEvent e) {
+
+        int x = 0; // will change for each pointer
+        int y = 0; // will change for each pointer
+        int maskedAction = e.getActionMasked();
+        int pointerCount = e.getPointerCount();
+        int cl_action = 0;
+
+        if (maskedAction == MotionEvent.ACTION_POINTER_DOWN) {
+            Log.d("debug", "action pointer down");
+            for (int size = pointerCount, i = 0; i < size; i++) {
+                int pointId = e.getPointerId(i);
+                x = (int) e.getX(i);
+                y = (int) e.getY(i);
+                addPointer(pointId, x, y); // FOR TESTING
+
+                if (pointId == 0) {
+                    cl_action = Constants.CLEV_MOUSE_DOWN_AUX_0;
+                } else if (pointId == 1) {
+                    cl_action = Constants.CLEV_MOUSE_DOWN_AUX_1;
+                } else if (pointId == 2) {
+                    cl_action = Constants.CLEV_MOUSE_DOWN_AUX_2;
+                }
+                Log.d("debug", "Sending :: POINTER DOWN :: " + cl_action + " through the stream...");
+                mStreamThreadHandler.obtainMessage(cl_action, x, y).sendToTarget();
+            }
+        } else if (maskedAction == MotionEvent.ACTION_POINTER_UP) {
+            Log.d("debug", "action pointer up");
+            for (int size = pointerCount, i = 0; i < size; i++) {
+                int pointId = e.getPointerId(i);
+                x = (int) e.getX(i);
+                y = (int) e.getY(i);
+                removePointer(pointId); // FOR TESTING
+
+                if (pointId == 0) {
+                    cl_action = Constants.CLEV_MOUSE_UP_AUX_0;
+                } else if (pointId == 1) {
+                    cl_action = Constants.CLEV_MOUSE_UP_AUX_1;
+                } else if (pointId == 2) {
+                    cl_action = Constants.CLEV_MOUSE_UP_AUX_2;
+                }
+                Log.d("debug", "Sending :: POINTER UP :: " + cl_action + " through the stream...");
+                mStreamThreadHandler.obtainMessage(cl_action, x, y).sendToTarget();
+            }
+        } else if (maskedAction == MotionEvent.ACTION_MOVE) {
+            Log.d("debug", "action move");
+            for (int size = pointerCount, i = 0; i < size; i++) {
+                int pointId = e.getPointerId(i);
+                x = (int) e.getX(i);
+                y = (int) e.getY(i);
+                addPointer(pointId, x, y);
+
+                if (pointId == 0) {
+                    cl_action = Constants.CLEV_MOUSE_MOVE_AUX_0;
+                } else if (pointId == 1) {
+                    cl_action = Constants.CLEV_MOUSE_MOVE_AUX_1;
+                } else if (pointId == 2) {
+                    cl_action = Constants.CLEV_MOUSE_MOVE_AUX_2;
+                }
+                Log.d("debug", "Sending :: MOVE :: " + cl_action + " through the stream...");
+                mStreamThreadHandler.obtainMessage(cl_action, x, y).sendToTarget();
+            }
+        } else {
+            Log.d("debug", "no case triggered for multitouch");
+            return true;
+        }
+
+        mStreamThreadHandler.obtainMessage(cl_action, x, y).sendToTarget();
+        return true;
+    }
+
+    /**
+     * Handles a single pointer in the form of a passed in MotionEvent
+     *
+     * @param e MotionEvent registered from android
+     * @return
+     */
+    public boolean handleSinglePointer(MotionEvent e) {
+
+        int x = (int) e.getX();
+        int y = (int) e.getY();
+        int action = e.getAction();
+        int cl_action = 0;
+
+        if (action == MotionEvent.ACTION_DOWN) {
+            cl_action = Constants.CLEV_MOUSE_DOWN;
+        } else if (action == MotionEvent.ACTION_UP) {
+            cl_action = Constants.CLEV_MOUSE_UP;
+        } else if (action == MotionEvent.ACTION_MOVE) {
+            cl_action = Constants.CLEV_MOUSE_MOVE;
+        } else if (action == MotionEvent.ACTION_SCROLL) {
+            if (android.os.Build.VERSION.SDK_INT >= 12) {
+                if (e.getAxisValue(MotionEvent.AXIS_VSCROLL) < 0) {
+                    cl_action = Constants.CLEV_SCROLL_DOWN;
+                } else {
+                    cl_action = Constants.CLEV_SCROLL_UP;
+                }
+            }
+        } else {
+            return true;
+        }
+
+        mStreamThreadHandler.obtainMessage(cl_action, x, y).sendToTarget();
+        return true;
+    }
+
 }
