70ea63e6c82eb8ac9022cfff48daf412a7da298f
Gregory Loden <gregory.loden@gmail.com>
2015-10-27 21:21:46 -0400

    update networking script

--- RemoteControlUnity/Foldit Remote Control/Assets/NetworkConScript.cs
+++ RemoteControlUnity/Foldit Remote Control/Assets/NetworkConScript.cs
@@ -18,9 +18,8 @@ public class NetworkConScript : MonoBehaviour {
 		socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 		socket.Connect(host, port);
 		byte[] buf = { 88, 3, 0, 0, 0, 0, 0, (byte)(screenwidth / 128), (byte)(screenwidth % 128), (byte)(screenheight / 128), (byte)(screenheight % 128), 0 };
-		Debug.Log(socket.Send(buf).ToString());
-		Debug.Log(socket.Receive(bytes).ToString());
-		dothing();
+		Debug.Log("Sent " + socket.Send(buf).ToString() + " bytes");
+		receiveToBytes();
 		Debug.Log("Connected");
 	}
 
@@ -28,38 +27,46 @@ public class NetworkConScript : MonoBehaviour {
 	void Update() {
 		if (timeWaited >= 2.0f) {
 			Debug.Log("Sending refresh");
-			Debug.Log(socket.Send(new byte[] { 88, 1, 0, 0, 0, 0, 0 }).ToString());
-			Debug.Log("Sent refresh");
-			Debug.Log(socket.Receive(bytes).ToString());
-			Debug.Log("Received screen");
-			dothing();
+			Debug.Log("Sent " + socket.Send(new byte[] { 88, 1, 0, 0, 0, 0, 0 }).ToString() + " bytes");
+			receiveToBytes();
 			timeWaited -= 2.0f;
 		}
 		timeWaited += Time.deltaTime;
 	}
 
-	void dothing() {
-		string s = "";
-		for (int q = 0; q < 256; q++)
-			s += ((int)(bytes[q])).ToString() + ", ";
-		Debug.Log(s);
+	void OnApplicationQuit() {
+		Debug.Log("Sending terminate");
+		Debug.Log("Sent " + socket.Send(new byte[] { 88, 2, 0, 0, 0, 0, 0 }).ToString() + " bytes");
+		int bytesReceived;
+		do {
+			bytesReceived = socket.Receive(bytes);
+			Debug.Log("****Received " + bytesReceived + " bytes for the screen****");
+		} while (bytesReceived > 0);
+		socket.Close();
+	}
+
+	void receiveToBytes() {
+		int bytesReceived;
+		do {
+			bytesReceived = socket.Receive(bytes);
+			Debug.Log("****Received " + bytesReceived + " bytes for the screen****");
+			string s = "";
+			int byteCount = bytesReceived < 256 ? bytesReceived : 256;
+			for (int q = 0; q < byteCount; q++)
+				s += ((int)(bytes[q])).ToString() + ", ";
+			Debug.Log(s);
+		} while (bytes[1] > 2);
 	}
 
 	public void ZoomIn() {
 		Debug.Log("Sending zoom in");
-		Debug.Log(socket.Send(new byte[] { 88, 4, 0, 1, 1, 1, 1 }).ToString());
-		Debug.Log("Sent zoom in");
-		Debug.Log(socket.Receive(bytes).ToString());
-		Debug.Log("Received screen");
-		dothing();
+		Debug.Log("Sent " + socket.Send(new byte[] { 88, 4, 0, 1, 1, 1, 1 }).ToString() + " bytes");
+		receiveToBytes();
 	}
 
 	public void ZoomOut() {
 		Debug.Log("Sending zoom out");
-		Debug.Log(socket.Send(new byte[] { 88, 3, 0, 1, 1, 1, 1 }).ToString());
-		Debug.Log("Sent zoom out");
-		Debug.Log(socket.Receive(bytes).ToString());
-		Debug.Log("Received screen");
-		dothing();
+		Debug.Log("Sent " + socket.Send(new byte[] { 88, 3, 0, 1, 1, 1, 1 }).ToString() + " bytes");
+		receiveToBytes();
 	}
 }
