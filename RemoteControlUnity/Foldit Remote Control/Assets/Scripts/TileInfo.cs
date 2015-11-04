337416a8b4b4a3919cf6dfa3d462262078656d80
Martha Hamlin <mlhamlin92@gmail.com>
2015-11-03 21:56:56 -0500

    Changing over rendering to hopefully more unity friendly version (fewer objects)

---
+++ RemoteControlUnity/Foldit Remote Control/Assets/Scripts/TileInfo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileInfo {
+
+    public int x { get; private set; }
+    public int y { get; private set; }
+    public Color32[] colors { get; private set; }
+    const int SIZE = 16;
+
+    public TileInfo(int inX, int inY, Color32[] inColors)
+    {
+        x = inX;
+        y = inY;
+        colors = inColors;
+    }
+
+    public TileInfo(int inX, int inY, Color32 inColor)
+    {
+        x = inX;
+        y = inY;
+        int colCount = SIZE * SIZE;
+
+        colors = new Color32[colCount];
+        for (int i = 0; i < colCount; i++)
+        {
+            colors[i] = inColor;
+        }
+    }
+}
--- RemoteControlUnity/Foldit Remote Control/Assets/Scripts/TileRenderController.cs
+++ RemoteControlUnity/Foldit Remote Control/Assets/Scripts/TileRenderController.cs
@@ -2,23 +2,22 @@
 using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using System;
 
 public class TileRenderController : MonoBehaviour {
 
-    public GameObject RenderTile;
     public RectTransform MyCanvas;
     public RectTransform MyPanel;
+    public RawImage Display;
+    public Texture2D Texture;
+
     public int Width { get; private set; }
     public int Height { get; private set; }
+
 	public const int TILE_SIZE = 16;
 	public const int TILE_SIZE_SQUARED = TILE_SIZE * TILE_SIZE;
-    private int TilesWide;
-    private int TilesTall;
-    private int TileCount;
 
-    private TileRenderer[] Tiles;
-
-    private List<TileRenderer> ChangedTiles;
+    private List<TileInfo> NewTiles;
 
 	public NetworkConScript networkConnection;
 
@@ -29,51 +28,38 @@ public class TileRenderController : MonoBehaviour {
         Rect panelRec = MyPanel.rect;
         Width = (int) panelRec.width;
         Height = (int) panelRec.height;
-        TilesWide = Width / TILE_SIZE;
-        TilesTall = Height / TILE_SIZE;
 
-        TileCount = TilesWide * TilesTall;
-        Tiles = new TileRenderer[TileCount];
-        ChangedTiles = new List<TileRenderer>();
+        Texture = new Texture2D(Width, Height);
+        NewTiles = new List<TileInfo>();
+        Display.texture = Texture;
 
-        for(int i = 0; i < TileCount; i++)
-        {
-            GameObject tile = Instantiate<GameObject>(RenderTile);
-            tile.name = "Tile (" + i % TilesWide + ", " + i / TilesWide + ")";
-            tile.transform.SetParent(MyPanel);
-            TileRenderer rend = tile.GetComponent<TileRenderer>();
-            //We actually need the tiles on the top to be at the end of the array
-            //Tiles[i % TilesWide + (TilesTall - 1 - (i / TilesWide)) * TilesWide] = rend;
-            Tiles[i] = rend;
-        }
-
-		networkConnection.StartWithTileRenderController(this);
-	}
+        //networkConnection.StartWithTileRenderController(this);
+    }
 
     public void SetTile(int x, int y, Color32[] colors, bool lores)
     {
-        int index = y * TilesWide + x;
-        //TODO: Does this make more sense on the server code? also efficency?
-        Tiles[index].ReadyTile(colors);
-        ChangedTiles.Add(Tiles[index]);
+        NewTiles.Add(new TileInfo(x, y, colors));
     }
 
     public void SetTile(int x, int y, Color32 color)
     {
-        int index = y * TilesWide + x;
-        Tiles[index].ReadyTile(color);
-        ChangedTiles.Add(Tiles[index]);
+        NewTiles.Add(new TileInfo(x, y, color));
     }
 
     public void Flush()
     {
 
-        foreach(TileRenderer tile in ChangedTiles)
+        foreach(TileInfo tile in NewTiles)
         {
-            tile.Flush();
+            drawTile(tile);
         }
+        Texture.Apply();
+        NewTiles.Clear();
+    }
 
-        ChangedTiles.Clear();
+    private void drawTile(TileInfo tile)
+    {
+        Texture.SetPixels32(tile.x * TILE_SIZE, tile.y * TILE_SIZE, TILE_SIZE, TILE_SIZE, tile.colors);
     }
 
     public void setPanelSize()
