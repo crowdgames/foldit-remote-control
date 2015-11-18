501b48bb5001033468d59ff2d7208fdff8736f2c
Ashley Sullivan <melodiouscacophony@gmail.com>
2015-11-17 20:17:34 -0500

    Hooked up skeleton functions to UI things that weren't the info panel or the mouse.

--- RemoteControlUnity/Foldit Remote Control/Assets/Scripts/UIInput.cs
+++ RemoteControlUnity/Foldit Remote Control/Assets/Scripts/UIInput.cs
@@ -3,16 +3,20 @@ using UnityEngine.UI;
 using System.Collections;
 
 // Developed by Elizabeth Renn
+// Developed by Ashley Sullivan
 
 public class UIInput : MonoBehaviour {
 	private bool isFirstConnection = true;
 	public InputField ipAddressInput;
 	public InputField requiredKeyInput;
 	public NetworkConScript connection;
+	
 
 	public void toggleUI(GameObject uiContainer) {
 		uiContainer.SetActive (!uiContainer.activeSelf);
 	}
+
+	// Connects to Foldit.
 
 	public void connectToFoldit(GameObject uiContainer) {
 		if (isFirstConnection) {
@@ -26,6 +30,38 @@ public class UIInput : MonoBehaviour {
 		string requiredKey = requiredKeyInput.text;
 		connection.connect (ipAddress, requiredKey);
 		uiContainer.SetActive(false);
+	}
+
+	/// 
+	/// SKELETON FUNCTIONS
+	/// 2SPOOKY4U UNTIL DEFINED PROPERLY
+
+
+	// Re-displays connection dialog.
+	public void changeIP() {
+
+		Debug.Log ("Change IP");
+	}
+
+	// Shows/loads tutorial.
+	public void showTutorial() {
+
+		Debug.Log ("Show tutorial");
+	}
+
+	// Toggles system keyboard.
+	public void toggleKeyboard() {
+
+		Debug.Log ("Toggle keyboard");
+	}
+
+
+	// Adjusts the zoom % to value according to slider value.
+	// (100 + slider value)%?
+	public void adjustZoom() {
+		// Skeleton functions.
+		Debug.Log ("Adjust zoom");
+
 	}
 }
 
