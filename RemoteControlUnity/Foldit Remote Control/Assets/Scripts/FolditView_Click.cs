aa3eaed999a76f727ef21e29d60a63e0d53edd76
Ashley Sullivan <melodiouscacophony@gmail.com>
2015-11-20 15:42:38 -0500

    quick change to script before pulling from master

--- RemoteControlUnity/Foldit Remote Control/Assets/Scripts/FolditView_Click.cs
+++ RemoteControlUnity/Foldit Remote Control/Assets/Scripts/FolditView_Click.cs
@@ -5,12 +5,17 @@ using System;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
+// Ashley Sullivan
+
+// Clicking in the Foldit view window behavior.
+
 public class FolditView_Click : MonoBehaviour, IPointerClickHandler {
 	// Drag to initialize
 	public GameObject networkCon;
 	// Use this for initialization
 	void Start () {
-		//GetComponent<TapGesture>().Tapped += clickInFoldit;
+		GetComponent<TapGesture>().Tapped += clickInFoldit;
+
 	}
 	
 	// Update is called once per frame
@@ -25,29 +30,29 @@ public class FolditView_Click : MonoBehaviour, IPointerClickHandler {
 	/// </summary>
 	public virtual void OnPointerClick(PointerEventData eventData)
 	{
-//		Vector2 position;
-//		RectTransformUtility.ScreenPointToLocalPointInRectangle(
-//			GetComponent<RectTransform>(), 
-//		    Input.mousePosition, 
-//		    Camera.main, 
-//		    out position);
+//		 
 		Vector2 localpos = Input.mousePosition;
-		//localpos = Vector2.Scale(localpos, new Vector2(1.375f, 1.32f));
 		Debug.Log ("Local point: " + localpos);
 		networkCon.GetComponent<NetworkConScript>().Tap(true, 
-		                                                Mathf.FloorToInt(localpos.x), 
-		                                                Mathf.FloorToInt(localpos.y));
+		                                                Mathf.Floor(localpos.x), 
+		                                                Mathf.Floor(localpos.y));
 		networkCon.GetComponent<NetworkConScript>().Tap(false, 
-		                                                Mathf.FloorToInt(localpos.x), 
-		                                                Mathf.FloorToInt(localpos.y));
+		                                                Mathf.Floor(localpos.x), 
+		                                                Mathf.Floor(localpos.y));
 	}
 
 	// Sends a click for the correct coordinates to Foldit
-	/**public void clickInFoldit(object sender, EventArgs e)
+	public void clickInFoldit(object sender, EventArgs e)
 	{
-		Vector2 position = (sender as TapGesture).ScreenPosition;
-		Debug.Log (position);
-	}**/
+		Vector2 localpos = (sender as TapGesture).ScreenPosition;
+		Debug.Log ("Local point: " + localpos);
+		networkCon.GetComponent<NetworkConScript>().Tap(true, 
+		                                                Mathf.Floor(localpos.x), 
+		                                                Mathf.Floor(localpos.y));
+		networkCon.GetComponent<NetworkConScript>().Tap(false, 
+		                                                Mathf.Floor(localpos.x), 
+		                                                Mathf.Floor(localpos.y));
+	}
 
 }
 
