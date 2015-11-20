8d52521129da66e16c1e89b4071ffdd968605120
smoss <Smoss117@gmail.com>
2015-11-20 17:08:02 -0500

    Can now click and drag things

--- RemoteControlUnity/Foldit Remote Control/Assets/Scripts/FolditView_Click.cs
+++ RemoteControlUnity/Foldit Remote Control/Assets/Scripts/FolditView_Click.cs
@@ -4,19 +4,24 @@ using TouchScript.Gestures;
 using System;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TouchScript.Gestures.Simple;
 
 // Ashley Sullivan
 
 // Clicking in the Foldit view window behavior.
 
-public class FolditView_Click : MonoBehaviour, IPointerClickHandler {
+public class FolditView_Click : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
 	// Drag to initialize
 	public GameObject networkCon;
 	// Use this for initialization
 	void Start () {
 		GetComponent<TapGesture>().Tapped += clickInFoldit;
+        //GetComponent<PressGesture>().Pressed += pressedHandler;
+        //GetComponent<SimplePanGesture>().Panned += panHandler;
+        //GetComponent<ReleaseGesture>().Released += releasedHandler;
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -56,5 +61,35 @@ public class FolditView_Click : MonoBehaviour, IPointerClickHandler {
 		                                                Mathf.FloorToInt(localpos.y));
 	}
 
+
+    private void releasedHandler(object sender, EventArgs e)
+    {
+        networkCon.GetComponent<NetworkConScript>().Tap(false, (int)Input.mousePosition.x, (int)Input.mousePosition.y);
+    }
+
+    private void pressedHandler(object sender, EventArgs e)
+    {
+        networkCon.GetComponent<NetworkConScript>().Tap(true, (int)Input.mousePosition.x, (int)Input.mousePosition.y);
+    }
+
+    private void panHandler(object sender, EventArgs e)
+    {
+        networkCon.GetComponent<NetworkConScript>().MouseMove((int)Input.mousePosition.x, (int)Input.mousePosition.y);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        networkCon.GetComponent<NetworkConScript>().Tap(true, (int)Input.mousePosition.x, (int)Input.mousePosition.y);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        networkCon.GetComponent<NetworkConScript>().Tap(false, (int)Input.mousePosition.x, (int)Input.mousePosition.y);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        networkCon.GetComponent<NetworkConScript>().MouseMove((int)Input.mousePosition.x, (int)Input.mousePosition.y);
+    }
 }
 
