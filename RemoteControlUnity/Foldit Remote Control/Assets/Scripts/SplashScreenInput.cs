a3616291eac3733c6cf0ad68c534247a72dd63b2
Elizabeth Renn <erenn@skillz.com>
2015-11-30 23:25:45 -0500

    Adding prelim splash screen and updating the build settings to include the multiple scenes

---
+++ RemoteControlUnity/Foldit Remote Control/Assets/Scripts/SplashScreenInput.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// Written by Elizabeth
+public class SplashScreenInput : MonoBehaviour {
+
+	public void startRemoteControl() {
+		Application.LoadLevel ("FolditRemoteControl"); 
+	}
+
+	public void showHelpWindow() {
+
+	}
+}
