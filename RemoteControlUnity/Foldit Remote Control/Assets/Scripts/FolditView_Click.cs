d0cd6e5d457c92c1b1b6abdfb341d8291f237671
Ashley Sullivan <melodiouscacophony@gmail.com>
2015-12-06 20:58:10 -0500

    Take dos, removed redundant functions

--- RemoteControlUnity/Foldit Remote Control/Assets/Scripts/FolditView_Click.cs
+++ RemoteControlUnity/Foldit Remote Control/Assets/Scripts/FolditView_Click.cs
@@ -9,6 +9,8 @@ using TouchScript.Gestures.Simple;
 // Ashley Sullivan
 // Sean Moss
 
+// 06 December 2015
+
 // Clicking in the Foldit view window behavior.
 
 public class FolditView_Click : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
@@ -25,15 +27,11 @@ public class FolditView_Click : MonoBehaviour, IPointerDownHandler, IPointerUpHa
 	void Start () {
         netConScript = networkCon.GetComponent<NetworkConScript>();
 		trc = tileRC.GetComponent<TileRenderController> ();
-
-		GetComponent<ScaleGesture> ().Scaled += scaleHandler;
-		GetComponent<PanGesture> ().Panned += panHandler;
-        GetComponent<ReleaseGesture>().Released += releasedHandler;
-        GetComponent<PressGesture>().Pressed += pressedHandler;
     }
 
     // Update is called once per frame
     void Update () {
+		// Checks for zooming via pinching.
 		if (Input.touchCount == 2) {	// Store both touches.
 			Touch touchZero = Input.GetTouch (0);
 			Touch touchOne = Input.GetTouch (1);
@@ -47,6 +45,7 @@ public class FolditView_Click : MonoBehaviour, IPointerDownHandler, IPointerUpHa
 			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
 		
 			// Find the difference in the distances between each frame.
+			// Practical purpose: Determine direction of zoom
 			float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
 
 			netConScript.Zoom(deltaMagnitudeDiff > 0);
@@ -54,34 +53,6 @@ public class FolditView_Click : MonoBehaviour, IPointerDownHandler, IPointerUpHa
 		}
 		
 	}
-	private void releasedHandler(object sender, EventArgs e)
-		
-	{
-		Debug.Log ("Release");
-		netConScript.Tap(false, (int)Input.mousePosition.x, (int)Input.mousePosition.y);
-    }
-
-    private void pressedHandler(object sender, EventArgs e)
-    {
-		Debug.Log ("Press");
-        netConScript.Tap(true, (int)Input.mousePosition.x, (int)Input.mousePosition.y);
-    }
-
-	// This is the pinch/nega-pinch to send to Foldit.
-	void scaleHandler (object sender, EventArgs e)
-	{
-		Debug.Log ("Zoom? " + (sender as ScaleGesture).LocalDeltaScale);
-		// If scale > 1, we zoomed in, otherwise we zoomed out.
-		netConScript.Zoom ((sender as ScaleGesture).LocalDeltaScale > 1);
-	}
-
-	// This is the local panning of the panel.
-	private void panHandler(object sender, EventArgs e)
-    {
-		Debug.Log ("Pan");
-		Vector3 delta = (sender as PanGesture).LocalDeltaPosition;
-		trc.dragPanel (new Vector2 (delta.x, delta.y));
-    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
@@ -95,18 +66,12 @@ public class FolditView_Click : MonoBehaviour, IPointerDownHandler, IPointerUpHa
 		netConScript.Tap(false, (int)adjusted.x, (int)adjusted.y);
     }
 
-	//public void OnDrag(PointerEventData eventData) {
-	//		Vector3 delta = eventData.delta;
-	//		trc.dragPanel (new Vector2 (delta.x, delta.y));
-	//	}
-
-
-    public void OnDrag(PointerEventData eventData)
+	public void OnDrag(PointerEventData eventData)
     {
 		if (Input.touchCount < 2) { // Either mouse input or single drag
 			Vector2 adjusted = trc.factorOutZoom (new Vector2 ((int)Input.mousePosition.x, (int)Input.mousePosition.y));
 			netConScript.MouseMove ((int)adjusted.x, (int)adjusted.y);
-		} else {
+		} else { // Pan around the ... panel
 			Vector3 delta = eventData.delta;
 			trc.dragPanel (new Vector2 (delta.x, delta.y));
 		}
