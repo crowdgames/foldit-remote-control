d654b83059654b4d8eaa42bec550af5638425319
Dhruv Shah <dhruvsshah@gmail.com>
2016-03-24 08:44:21 -0400

    Added a test file for unit test cases on LoginIpActivity

---
+++ RemoteControlAndroid/app/src/test/java/it/fold/remotecontrolandroid/LoginIPActivityTest.java
@@ -0,0 +1,41 @@
+package it.fold.remotecontrolandroid;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Dhruv on 3/21/2016.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class LoginIPActivityTest {
+
+    private LoginIPPresenter presenter;
+    @Mock
+    private LoginView view;
+
+    @Before
+    public void setUp() throws Exception {
+        presenter = new LoginIPPresenter(view);
+
+    }
+
+    @Test
+    public void shouldShowErrorMessageWhenIPFieldIsEmpty() throws Exception {
+        when(view.getIPAddress()).thenReturn("");
+        presenter.attemptLogin();
+
+        verify(view).showIPError(R.string.error_field_required);
+    }
+
+    @Test
+    public void shouldShowErrorMessageWhenOptionalKeyFieldIsInvalid() throws Exception {
+
+    }
+}
