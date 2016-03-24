7c8e672ad36d1e56160692acc1e6bd9a6ca456da
Dhruv Shah <dhruvsshah@gmail.com>
2016-03-24 08:42:41 -0400

    Added LoginView  Interface

---
+++ RemoteControlAndroid/app/src/main/java/it/fold/remotecontrolandroid/LoginView.java
@@ -0,0 +1,11 @@
+package it.fold.remotecontrolandroid;
+
+/**
+ * Created by Dhruv on 3/21/2016.
+ */
+public interface LoginView {
+
+    String getIPAddress();
+
+    void showIPError(int resId);
+}
