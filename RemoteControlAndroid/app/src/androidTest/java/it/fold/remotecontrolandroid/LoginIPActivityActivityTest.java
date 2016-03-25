d9f8a225b51dd7f311f201695d58eb8693c927f9
Ruta Gadgil <gadgil.r@husky.neu.edu>
2016-03-25 10:41:26 -0400

    adding more UI Tests - Espresso

--- RemoteControlAndroid/app/src/androidTest/java/it/fold/remotecontrolandroid/LoginIPActivityActivityTest.java
+++ RemoteControlAndroid/app/src/androidTest/java/it/fold/remotecontrolandroid/LoginIPActivityActivityTest.java
@@ -3,6 +3,8 @@ package it.fold.remotecontrolandroid;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,24 +19,42 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * @author - Ruta
  */
 @RunWith(AndroidJUnit4.class)
-  public class ApplicationTest {
-    public final String TAG = "ApplicationTest";
+  public class LoginIPActivityActivityTest {
+    public final String TAG = "LoginIPActivityActivityTest";
     @Rule
     public ActivityTestRule<LoginIPActivity> loginIPActivityActivityTestRule = new ActivityTestRule(LoginIPActivity.class);
 
     @Test
-    public void ensureTextChangesWork() {
-        //Log.d(TAG, "test run!!");
+    public void ensureIPAddressAcceptsInput() {
         // Type text and then press the button.
         onView(withId(R.id.ip))
                 .perform(typeText("123"), closeSoftKeyboard());
         onView(withId(R.id.IP_sign_in_button)).perform(click());
-
+    }
+    @Test
+    public  void ensureIPAddressChanges(){
         // Check that the text was changed.
+        onView(withId(R.id.ip))
+                .perform(typeText("123"), closeSoftKeyboard());
+        onView(withId(R.id.IP_sign_in_button)).perform(click());
         onView(withId(R.id.ip)).check(matches(withText("123")));
+        Log.d(TAG, "test - LoginIPActivity accepts IP as numeric values");
+    }
+
+    @Test
+    public  void ensureOptionalPasswordChanges(){
+        // Check that the text was changed.
+        onView(withId(R.id.password))
+                .perform(typeText("qwerty"), closeSoftKeyboard());
+        onView(withId(R.id.IP_sign_in_button)).perform(click());
+        onView(withId(R.id.password)).check(matches(withText("qwerty")));
+        Log.d(TAG, "test - LoginIPActivity accepts password");
     }
 }
