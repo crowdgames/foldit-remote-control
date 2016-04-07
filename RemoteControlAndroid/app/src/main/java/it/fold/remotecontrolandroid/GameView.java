ee6e10decab71717e36ea501c2f659527cb6d629
Dhruv Shah <dhruvsshah@gmail.com>
2016-04-07 19:43:45 -0400

    Added game view interface to call activity methods from list adapter

---
+++ RemoteControlAndroid/app/src/main/java/it/fold/remotecontrolandroid/GameView.java
@@ -0,0 +1,16 @@
+package it.fold.remotecontrolandroid;
+
+import android.view.View;
+
+/**
+ * Created by Dhruv on 4/7/2016.
+ */
+public interface GameView {
+    void onLeftClickButton(View v);
+
+    void onRightClickButton(View v);
+
+    void onMiddleClickButton(View v);
+
+    void bringUpKeyboard(View v);
+}
